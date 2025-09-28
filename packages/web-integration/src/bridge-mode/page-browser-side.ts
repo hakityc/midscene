592de50159588ad202fa8df5ad81e19665854dd4
tangyicong <yicong_tang@lebo.cn>
2025-09-28 17:48:43 +0800

    fix(bridge): 修复获取当前标签页ID的逻辑，改为获取最后一个标签页的ID

--- packages/web-integration/src/bridge-mode/page-browser-side.ts
+++ packages/web-integration/src/bridge-mode/page-browser-side.ts
@@ -148,11 +148,12 @@ export class ExtensionBridgePageBrowserSide extends ChromeExtensionProxyPage {
       forceSameTabNavigation: true,
     },
   ) {
-    const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
-    const tabId = tabs[0]?.id;
+    const tabs = await chrome.tabs.query({});
+    const lastTabIndx = tabs.length - 1;
+    const tabId = tabs[lastTabIndx]?.id;
     assert(tabId, 'failed to get tabId');
 
-    this.onLogMessage(`Connected to current tab: ${tabs[0]?.url}`, 'log');
+    this.onLogMessage(`Connected to current tab: ${tabs[lastTabIndx]?.url}`, 'log');
 
     if (options?.forceSameTabNavigation) {
       this.forceSameTabNavigation = true;
